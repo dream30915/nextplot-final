147a8183a279f486011a12707d1879d72ea846df
Dump <dream30915@gmail.com>
2025-10-08 09:05:04 +0700

    config: add supabase public url & anon key
    Signed-off-by: Dump <dream30915@gmail.com>

---
+++ src/config.supabase.js
@@ -0,0 +1,3 @@
+// Supabase public config (safe to expose anon key)
+export const SUPABASE_URL = 'https://jwzyeivhbrttsqubwjcf.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3enllaXZoYnJ0dHNxdWJ3amNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3NTkyNTAsImV4cCI6MjA3NTMzNTI1MH0.N_dQ6MgSB-4AJ2NIxzSppRIMV2Dkb-qSpip5Be-Uwxw';
